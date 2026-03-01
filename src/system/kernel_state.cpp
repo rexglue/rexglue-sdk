ec59dc7ea10270c1cdb7ff924e27a42e90936a81
Tom <1568512+tomcl7@users.noreply.github.com>
2026-03-01 12:20:29 -0500

    refactor: decouple kernel_state.cpp from rexkernel
    Hardcode ordinal values and use rex::ppc::kExLoadedImageNameSize from function.h instead of importing rexkernel headers.

--- src/system/kernel_state.cpp
+++ src/system/kernel_state.cpp
@@ -14,9 +14,8 @@
 #include <fmt/format.h>
 
 #include <rex/assert.h>
-#include <rex/kernel/xboxkrnl/module.h>
-#include <rex/kernel/xboxkrnl/ordinals.h>
 #include <rex/logging.h>
+#include <rex/ppc/function.h>
 #include <rex/runtime.h>
 #include <rex/stream.h>
 #include <rex/string.h>
@@ -336,7 +335,7 @@ void KernelState::SetExecutableModule(object_ref<UserModule> module) {
 
   // Setup the kernel's XexExecutableModuleHandle field.
   auto export_entry = emulator_->processor()->export_resolver()->GetExportByOrdinal(
-      "xboxkrnl.exe", kernel::xboxkrnl::ordinals::XexExecutableModuleHandle);
+      "xboxkrnl.exe", 0x0193 /* XexExecutableModuleHandle */);
   if (export_entry) {
     assert_not_zero(export_entry->variable_ptr);
     auto variable_ptr = memory_->TranslateVirtual<be<uint32_t>*>(export_entry->variable_ptr);
@@ -345,11 +344,11 @@ void KernelState::SetExecutableModule(object_ref<UserModule> module) {
 
   // Setup the kernel's ExLoadedImageName field
   export_entry = emulator_->processor()->export_resolver()->GetExportByOrdinal(
-      "xboxkrnl.exe", kernel::xboxkrnl::ordinals::ExLoadedImageName);
+      "xboxkrnl.exe", 0x01AF /* ExLoadedImageName */);
   if (export_entry) {
     char* variable_ptr = memory_->TranslateVirtual<char*>(export_entry->variable_ptr);
     rex::string::util_copy_truncating(variable_ptr, executable_module_->path(),
-                                      kernel::xboxkrnl::XboxkrnlModule::kExLoadedImageNameSize);
+                                      kExLoadedImageNameSize);
   }
 
   // Spin up deferred dispatch worker.
