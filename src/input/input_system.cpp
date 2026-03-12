d8865beb45a6eb7a18aa0a3b781fb3f366759324
Sanjay Govind <sanjay.govind9@gmail.com>
2026-03-13 09:01:07 +1300

    feat(input): port xinput driver from xenia

--- src/input/input_system.cpp
+++ src/input/input_system.cpp
@@ -15,8 +15,13 @@
 #include <rex/input/input_system.h>
 #include <rex/input/nop/nop_input_driver.h>
 #include <rex/input/sdl/sdl_input_driver.h>
+#include <rex/input/xinput/xinput_input_driver.h>
 #include <rex/logging.h>
 
+REXCVAR_DEFINE_STRING(input_backend, "sdl", "Input", "Input backend: sdl, xinput")
+    .allowed({"sdl", "xinput"});
+
+REXCVAR_DEFINE_BOOL(guide_button, false, "Input", "Enable guide button pass-through");
 namespace rex::input {
 
 InputSystem::InputSystem(rex::ui::Window* window) : window_(window) {}
@@ -105,9 +110,20 @@ std::unique_ptr<InputSystem> CreateDefaultInputSystem(bool tool_mode) {
   auto input = std::make_unique<InputSystem>(nullptr);
 
   if (!tool_mode) {
-    auto sdl_driver = std::make_unique<sdl::SDLInputDriver>(nullptr, 0);
-    if (sdl_driver->Setup() == X_STATUS_SUCCESS) {
-      input->AddDriver(std::move(sdl_driver));
+#if REX_PLATFORM_WIN32
+    if (REXCVAR_GET(input_backend) == "xinput") {
+      auto xinput_driver = std::make_unique<xinput::XinputInputDriver>(nullptr, 0);
+      if (xinput_driver->Setup() == X_STATUS_SUCCESS) {
+        input->AddDriver(std::move(xinput_driver));
+      }
+    }
+#endif
+
+    if (REXCVAR_GET(input_backend) == "sdl") {
+      auto sdl_driver = std::make_unique<sdl::SDLInputDriver>(nullptr, 0);
+      if (sdl_driver->Setup() == X_STATUS_SUCCESS) {
+        input->AddDriver(std::move(sdl_driver));
+      }
     }
   }
 
